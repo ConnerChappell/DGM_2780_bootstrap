9715260b8d47d9f6fdd4fb644172526522de8e02
Conner Chappell <conner.chappell123@gmail.com>
2020-12-18 19:53:48 -0700

    Added greensock animation to logo and nav items

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Greensock logo animation
+gsap.from(".logo", {duration: 2.5, opacity: 0, scale: 0.3, ease:"back"});
+
+// Greensock nav-item animation
+gsap.from(".nav-item", {duration: 1, opacity: 0, delay: .5, y: 150, stagger: 0.25});
